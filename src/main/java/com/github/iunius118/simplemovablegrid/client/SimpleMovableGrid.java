bd92f6e236c764ca0b617b98c69ea4103abf1c52
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2022-05-01 03:09:00 +0900

    Translation keys for messages

--- src/main/java/com/github/iunius118/simplemovablegrid/client/SimpleMovableGrid.java
+++ src/main/java/com/github/iunius118/simplemovablegrid/client/SimpleMovableGrid.java
@@ -11,13 +11,32 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Vec3i;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtIo;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 
 public class SimpleMovableGrid implements ClientModInitializer {
     public static final String MOD_ID = "simplemovablegrid";
+    private static final String MOD_NAME = "Simple Movable Grid";
+
+    public static final Logger LOGGER = LogManager.getFormatterLogger(MOD_NAME);
 
     @Override
     public void onInitializeClient() {
@@ -37,6 +56,7 @@ public class SimpleMovableGrid implements ClientModInitializer {
     private void bindKeys() {
         KeyMapping keyEnable = KeyBindingHelper.registerKeyBinding(createKeyBinding("enable", InputConstants.UNKNOWN.getValue(), "main"));
         KeyMapping keySetPosition = KeyBindingHelper.registerKeyBinding(createKeyBinding("setPosition", InputConstants.UNKNOWN.getValue(), "main"));
+        // KeyMapping keySaveStructure = KeyBindingHelper.registerKeyBinding(createKeyBinding("saveStructure", InputConstants.UNKNOWN.getValue(), "main"));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (keyEnable.consumeClick()) {
@@ -45,7 +65,10 @@ public class SimpleMovableGrid implements ClientModInitializer {
                 AutoConfig.getConfigHolder(ModConfig.class).save();
 
                 LocalPlayer player = client.player;
-                if (player != null) player.sendMessage(createChatMessage("Grid: " + (enabled ? "shown" : "hidden")), null);
+                if (player != null) {
+                    MutableComponent message = Component.translatable(enabled ? "simplemovablegrid.grid.shown" : "simplemovablegrid.grid.hidden");
+                    player.displayClientMessage(createChatMessage(message), false);
+                }
             }
 
             while (keySetPosition.consumeClick()) {
@@ -56,16 +79,68 @@ public class SimpleMovableGrid implements ClientModInitializer {
                 Vec3i pos = config.setPos(player.getBlockX(), player.getBlockY(), player.getBlockZ());
                 AutoConfig.getConfigHolder(ModConfig.class).save();
 
-                client.player.sendMessage(createChatMessage("Position is set to (" + pos.toShortString() + ")"), null);
+                MutableComponent message = Component.translatable("simplemovablegrid.setPosition.success", pos.toShortString());
+                client.player.displayClientMessage(createChatMessage(message), false);
             }
+
+            /*
+            while (keySaveStructure.consumeClick()) {
+                ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
+                if (!config.enabled()) continue;
+                // Save the structure only when the grid is enabled
+                LocalPlayer player = client.player;
+                if (player == null) continue;
+                Level level = player.getLevel();
+                if (level == null) continue;
+
+                BlockPos pos = new BlockPos(config.getPos());
+                ResourceLocation structureName = new ResourceLocation(MOD_ID, getTimeStamp());
+                boolean result = saveStructure(level, pos, structureName);
+
+                MutableComponent message = Component.translatable(result ? "structure_block.save_success" : "structure_block.save_failure", structureName.toString());
+                client.player.displayClientMessage(createChatMessage(message), false);
+            }
+            // */
         });
+    }
+
+    private String getTimeStamp() {
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss_SSS");
+        return simpleDateFormat.format(timestamp);
+    }
+
+    private boolean saveStructure(Level level, BlockPos pos, ResourceLocation structureName) {
+        StructureTemplate structureTemplate = new StructureTemplate();
+        structureTemplate.fillFromWorld(level, pos, new Vec3i(32, 32, 32), false, Blocks.STRUCTURE_VOID);
+        Path gameDirectory = Minecraft.getInstance().gameDirectory.toPath();
+        Path saveDirPath = gameDirectory.resolve(Path.of(MOD_ID, "generated", structureName.getNamespace(), "structures"));
+        Path saveFilePath = saveDirPath.resolve(Path.of(structureName.getPath() + ".nbt"));
+
+        try {
+            Files.createDirectories(saveDirPath);
+        } catch (Exception e) {
+            LOGGER.error((String)"Failed to create parent directory: {}", saveDirPath);
+            return false;
+        }
+
+        CompoundTag compoundTag = structureTemplate.save(new CompoundTag());
+
+        try {
+            FileOutputStream outputStream = new FileOutputStream(saveFilePath.toFile());
+            NbtIo.writeCompressed(compoundTag, outputStream);
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true;
     }
 
     private KeyMapping createKeyBinding(String name, int key, String category) {
         return new KeyMapping("key." + MOD_ID + "." + name, key, "key.categories." + MOD_ID + "." + category);
     }
 
-    private Component createChatMessage(String message) {
-        return new TextComponent("").append(new TextComponent("[SimpleMovableGrid] ").withStyle(ChatFormatting.YELLOW)).append(new TextComponent(message).withStyle(ChatFormatting.RESET));
+    private Component createChatMessage(MutableComponent message) {
+        return Component.literal("").append(Component.literal("[Simple Movable Grid] ").withStyle(ChatFormatting.YELLOW)).append(message.withStyle(ChatFormatting.RESET));
     }
 }
